d09f124fa54017b9f1ab972977a2e166e42e89ff
Andrew Wong <andrew.wong.0101@gmail.com>
2018-10-20 21:34:33 +0800

    Add LIS

--- lis.py
+++ lis.py
@@ -1,12 +1,25 @@
 """
 Input = a list 
 Output = integer 
-ex. [5,7,4,-3,9,1,10,4,5,8,9,3] >>  5
+ex. [5,7,4,-3,9,1,10,4,5,8,9,3] >>  6
+Complexity = O(n^2)
 """
 
-def LIS(input):
-	# Input = list [1,2,....]
-	n = len(input)
+
+def LIS(x):
+	# x = list [1,2,....]
+	n = len(x)
+	array = [1]*n
+	for i in range(1,n):
+		for j in range(0,i):
+			if x[i]>x[j] and array[i]<array[j]+1:
+				array[i] = array[j]+1
+	maximum = 0
 	for i in range(n):
-		for j in range(i-1):
-			
+		maximum= max(array[i],maximum)
+	return maximum
+
+test1 = [5,7,4,-3,9,1,10,4,5,8,9,3]
+print (LIS(test1))
+test2 = [10, 22, 9, 33, 21, 50, 41, 60] 
+print (LIS(test2))
