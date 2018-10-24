40148836da2c8a39f2c80003014e330d5b870c40
Andrew Wong <andrew.wong.0101@gmail.com>
2018-10-24 17:20:52 +0800

    Initial commit

--- lcs.py
+++ lcs.py
@@ -1,19 +1,13 @@
 # Use recursive and dynamic programming to test runtime
-def lcs_recursive(x,y):
-	m = len(x)
-	n = len(y)
+def lcs_recursive(x,y,m,n):
 	if m ==0 or n==0:
 		return 0
 	elif x[m-1] == y[n-1]:
-		m-=1
-		n-=1
-		return lcs_recursive(x[:-1],y[:-1])
+		return lcs_recursive(x,y,m-1,n-1)
 	else:
-		return max(lcs_recursive(x[:-1],y),lcs_recursive(x,y[:-1]))
+		return max(lcs_recursive(x,y,m-1,n),lcs_recursive(x,y,m,n-1))
 
 
 X = "AGGTAB"
 Y = "GXTXAYB"
-print ("Length of LCS is ", lcs_recursive(X,Y))
-a = [1,2,3,4]
-print (a[:-1])
+print ("Length of LCS is ", lcs_recursive(X,Y,6,7))
