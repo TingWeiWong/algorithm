7d194630a9f0ef07421a4c2b02e4829a276d9f38
Andrew Wong <andrew.wong.0101@gmail.com>
2018-10-26 14:30:03 +0800

    intiai commit

--- lcs.py
+++ lcs.py
@@ -3,7 +3,7 @@ def lcs_recursive(x,y,m,n):
 	if m ==0 or n==0:
 		return 0
 	elif x[m-1] == y[n-1]:
-		return lcs_recursive(x,y,m-1,n-1)
+		return lcs_recursive(x,y,m-1,n-1)+1
 	else:
 		return max(lcs_recursive(x,y,m-1,n),lcs_recursive(x,y,m,n-1))
 
