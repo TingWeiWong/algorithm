0016d7d85356faa1a5f2ba9b1ca6a3331ee54541
Andrew Wong <andrew.wong.0101@gmail.com>
2018-11-28 12:46:35 +0800

    Add dynamic

---
+++ xcode/c++_xcode/dynamic.cpp
@@ -0,0 +1,12 @@
+//
+//  dynamic.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 11/27/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#include "dynamic.hpp"
+
+#include <iostream>
+
---
+++ xcode/c++_xcode/dynamic.hpp
@@ -0,0 +1,14 @@
+//
+//  dynamic.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 11/27/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#ifndef dynamic_hpp
+#define dynamic_hpp
+
+#include <stdio.h>
+
+#endif /* dynamic_hpp */
