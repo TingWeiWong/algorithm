e5cf3524da0481999c8fe567bfa1d5a430d273fd
Andrew Wong <andrew.wong.0101@gmail.com>
2018-11-30 08:52:39 +0800

    Add vector

---
+++ xcode/c++_xcode/vector.cpp
@@ -0,0 +1,9 @@
+//
+//  vector.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 11/30/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#include "vector.hpp"
---
+++ xcode/c++_xcode/vector.hpp
@@ -0,0 +1,14 @@
+//
+//  vector.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 11/30/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#ifndef vector_hpp
+#define vector_hpp
+
+#include <stdio.h>
+
+#endif /* vector_hpp */
