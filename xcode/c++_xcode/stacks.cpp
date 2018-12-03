cb24fcd12504a6e4ac55399156a65c955c9dfda5
Andrew Wong <andrew.wong.0101@gmail.com>
2018-12-03 10:56:45 +0800

    Add stack

---
+++ xcode/c++_xcode/stacks.cpp
@@ -0,0 +1,12 @@
+//
+//  stacks.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/3/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#include "stacks.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
---
+++ xcode/c++_xcode/stacks.hpp
@@ -0,0 +1,14 @@
+//
+//  stacks.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/3/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#ifndef stacks_hpp
+#define stacks_hpp
+
+#include <stdio.h>
+
+#endif /* stacks_hpp */
