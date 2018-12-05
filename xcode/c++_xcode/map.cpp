ae1d57d12967abdb031b7d5a5ec8d4bafe249b41
Andrew Wong <andrew.wong.0101@gmail.com>
2018-12-05 16:54:09 +0800

    Add map

---
+++ xcode/c++_xcode/map.cpp
@@ -0,0 +1,10 @@
+//
+//  map.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/5/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#include "map.hpp"
+#include <map>
---
+++ xcode/c++_xcode/map.hpp
@@ -0,0 +1,14 @@
+//
+//  map.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/5/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#ifndef map_hpp
+#define map_hpp
+
+#include <stdio.h>
+
+#endif /* map_hpp */
