4d71698dabffe1f8072c3d1431c7000b05f37e7e
Andrew Wong <andrew.wong.0101@gmail.com>
2018-12-05 16:39:32 +0800

    Add Set

--- xcode/c++_xcode/set.cpp
+++ xcode/c++_xcode/set.cpp
@@ -12,9 +12,19 @@
 #include <string>
 using namespace std;
 
+/*
+ We cannot modify the elements using iterators because if you modify the element value then internal data structure of std::set will get corrupt and it will not remain balanced binary search tree. Hence further additions and find operations will not work properly.
+ */
 void addSet(){
-    set<string> hi;
-    hi.insert("s");
-    hi.insert("a");
-    cout << "The set contains " << &hi;
+    set<string> SetofNumbers;
+    SetofNumbers.insert("first");
+    SetofNumbers.insert("second");
+    SetofNumbers.insert("third");
+    SetofNumbers.insert("fourth");
+    cout << "The size of set = " << SetofNumbers.size() << endl;
+    for (set<string>::iterator it = SetofNumbers.begin(); it != SetofNumbers.end(); it++){
+        cout << "Elements of set = " << *it << endl;
+    }
 }
+
+
