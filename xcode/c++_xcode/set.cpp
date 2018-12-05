5fc69cf4ce910a3bf0c3806caad5b5f312adbbcc
Andrew Wong <andrew.wong.0101@gmail.com>
2018-12-05 09:49:09 +0800

    Add set.cpp

---
+++ xcode/c++_xcode/set.cpp
@@ -0,0 +1,10 @@
+//
+//  set.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/5/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#include "set.hpp"
+#include <set>
---
+++ xcode/c++_xcode/set.hpp
@@ -0,0 +1,14 @@
+//
+//  set.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 12/5/18.
+//  Copyright © 2018 Andrew . All rights reserved.
+//
+
+#ifndef set_hpp
+#define set_hpp
+
+#include <stdio.h>
+
+#endif /* set_hpp */
