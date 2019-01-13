92e6ec9ade341c8cc0f2947f2864dd6dbe81fbe1
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-13 20:34:30 +0800

    initial commit

---
+++ xcode/c++_xcode/class.cpp
@@ -0,0 +1,11 @@
+//
+//  class.cpp
+//  c++_xcode
+//
+//  Created by Andrew on 1/13/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "class.hpp"
+#include <iostream>
+
---
+++ xcode/c++_xcode/class.hpp
@@ -0,0 +1,14 @@
+//
+//  class.hpp
+//  c++_xcode
+//
+//  Created by Andrew on 1/13/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef class_hpp
+#define class_hpp
+
+#include <stdio.h>
+
+#endif /* class_hpp */
