10d23ad81f563f4a4c1da4a53d0c653498f67be9
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-18 21:03:39 +0800

    finish struct

--- xcode/c++_xcode/pointers.cpp
+++ xcode/c++_xcode/pointers.cpp
@@ -9,4 +9,20 @@
 #include "pointers.hpp"
 #include <iostream>
 #include <string>
-
+using namespace std;
+struct Date{
+    int month;
+    int date;
+    int daysInMonth(){
+        if (month == 4 || month == 6 || month == 9 || month == 11){
+            return 30;
+        }else if (month == 2){
+            return 28;
+        }else{
+            return 31;
+        }
+    }
+    string toString(){
+        return to_string(month) + "/" + to_string(date);
+    }
+};
