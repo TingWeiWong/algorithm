42e732f8d110b797505a0399d962adfbe7b8ab2b
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-18 20:51:50 +0800

    initial commit

---
+++ xcode/c++_xcode/pointers.cpp
@@ -0,0 +1,12 @@
+//
+//  pointers.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/18/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "pointers.hpp"
+#include <iostream>
+#include <string>
+
---
+++ xcode/c++_xcode/pointers.hpp
@@ -0,0 +1,14 @@
+//
+//  pointers.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/18/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef pointers_hpp
+#define pointers_hpp
+
+#include <stdio.h>
+
+#endif /* pointers_hpp */
