29fef552bc18005ff2c339871f88c58ca00e9f31
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-19 14:03:38 +0800

    initial commit

---
+++ xcode/c++_xcode/linkedList.cpp
@@ -0,0 +1,9 @@
+//
+//  linkedList.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/19/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "linkedList.hpp"
---
+++ xcode/c++_xcode/linkedList.hpp
@@ -0,0 +1,14 @@
+//
+//  linkedList.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/19/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef linkedList_hpp
+#define linkedList_hpp
+
+#include <stdio.h>
+
+#endif /* linkedList_hpp */
