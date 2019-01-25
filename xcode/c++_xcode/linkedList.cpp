29148f66284e580e8d1ac6d2d4fb7c986ecb050f
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-25 15:28:01 +0800

    initial commit

--- xcode/c++_xcode/linkedList.cpp
+++ xcode/c++_xcode/linkedList.cpp
@@ -23,11 +23,27 @@ ListNode* head;
 
 // Point middle to new node
 
-void insert(int new_data){
+void fourNode(){
     // head points to a newly created node
     head = new ListNode;
     head -> data = 1;
     // middle and tail points to where head is pointing
     middle = head;
     tail = head;
+    // head points to a new node
+    head = new ListNode;
+    head -> data = 2;
+    // Make the middle pointer points to where head is pointing
+    middle -> next = head;
+    middle = middle -> next;
+    
+    // head points to a new node
+    head = new ListNode;
+    head -> data = 3;
+    middle -> next = head;
+    head = new ListNode;
+    middle = middle -> next;
+    head -> data = 4;
+    middle -> next = head;
+    head -> next = nullptr;
 }
