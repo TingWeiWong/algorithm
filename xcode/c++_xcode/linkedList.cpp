fee68b32a5ac43648f0285c867b1cfa02c50170b
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-25 15:18:07 +0800

    initial commit

--- xcode/c++_xcode/linkedList.cpp
+++ xcode/c++_xcode/linkedList.cpp
@@ -16,15 +16,18 @@ struct ListNode {
     int data;
     ListNode* next;
 };
-
+// Creating pointers middle ...
 ListNode* middle;
 ListNode* tail;
 ListNode* head;
 
-
-
-
+// Point middle to new node
 
 void insert(int new_data){
-    
+    // head points to a newly created node
+    head = new ListNode;
+    head -> data = 1;
+    // middle and tail points to where head is pointing
+    middle = head;
+    tail = head;
 }
