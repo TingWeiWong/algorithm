b24c8283525196556b34e2ba48d986bc28b86509
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-27 11:50:07 +0800

    Add array

---
+++ xcode/c++_xcode/array.cpp
@@ -0,0 +1,12 @@
+//
+//  array.cpp
+//  c++_xcode
+//
+//  Created by Andrew on 1/27/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "array.hpp"
+#include <iostream>
+using namespace std;
+
---
+++ xcode/c++_xcode/array.hpp
@@ -0,0 +1,14 @@
+//
+//  array.hpp
+//  c++_xcode
+//
+//  Created by Andrew on 1/27/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef array_hpp
+#define array_hpp
+
+#include <stdio.h>
+
+#endif /* array_hpp */
