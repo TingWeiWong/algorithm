75699eb7639fe71318ea832b2abadb8d337c4c76
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-27 12:08:27 +0800

    Add comments for array

--- xcode/c++_xcode/array.cpp
+++ xcode/c++_xcode/array.cpp
@@ -10,3 +10,17 @@
 #include <iostream>
 using namespace std;
 
+/*
+ Arrays
+ 
+ type name[length];
+ statically stored, can never be resized
+ memory does not automatically have to be free, will be automatically released
+ int homework[7]
+ 
+ type* name = new type[length];
+ dynamically stored (heap allocated) array
+ The variable that refers to the array is a pointer
+ The memory allocated for the array must be released or there will be memory leak
+ int* homework = new int[7]
+ */
