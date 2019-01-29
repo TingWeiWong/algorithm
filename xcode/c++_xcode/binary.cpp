421edb865988a712298a49d51ee0d5db2ce240b2
Andrew Wong <andrew.wong.0101@gmail.com>
2019-01-29 13:35:54 +0800

    Add binary

---
+++ xcode/c++_xcode/binary.cpp
@@ -0,0 +1,9 @@
+//
+//  binary.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/29/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "binary.hpp"
---
+++ xcode/c++_xcode/binary.hpp
@@ -0,0 +1,14 @@
+//
+//  binary.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 1/29/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef binary_hpp
+#define binary_hpp
+
+#include <stdio.h>
+
+#endif /* binary_hpp */
