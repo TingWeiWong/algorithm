fadd5ec4e718a16d6d50d35d7d9df4c26aaad923
Andrew Wong <andrew.wong.0101@gmail.com>
2019-02-11 00:36:36 +0800

    initial commit

---
+++ xcode/c++_xcode/inheritance.cpp
@@ -0,0 +1,9 @@
+//
+//  inheritance.cpp
+//  c++_xcode
+//
+//  Created by Andrew on 2/11/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "inheritance.hpp"
---
+++ xcode/c++_xcode/inheritance.hpp
@@ -0,0 +1,14 @@
+//
+//  inheritance.hpp
+//  c++_xcode
+//
+//  Created by Andrew on 2/11/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef inheritance_hpp
+#define inheritance_hpp
+
+#include <stdio.h>
+
+#endif /* inheritance_hpp */
