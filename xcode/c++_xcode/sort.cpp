37f99ffccfe7ae91f06019a62214fd0d7ab81415
Andrew Wong <andrew.wong.0101@gmail.com>
2019-02-15 23:30:14 +0800

    Add selectionSort

--- xcode/c++_xcode/sort.cpp
+++ xcode/c++_xcode/sort.cpp
@@ -27,3 +27,19 @@ void bogoSort(vector<int>& v){
         random_shuffle(v.begin(), v.end());
     }
 }
+
+void selectionSort(vector<int>& v){
+    for (int i = 0; i < v.size() - 1; i++){
+        int min = i;
+        for (int j = i + 1; j < v.size(); j++){
+            if (v[j] < v[min]){
+                min = j;
+            }
+        }
+        if (i != min){
+            int temp = v[i];
+            v[i] = v[min];
+            v[min] = temp;
+        }
+    }
+}
