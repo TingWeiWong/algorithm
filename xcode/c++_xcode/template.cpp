40f0c4ce2539da567619a36c77d30320413d3456
Andrew Wong <andrew.wong.0101@gmail.com>
2019-02-17 12:20:11 +0800

    Add template

---
+++ xcode/c++_xcode/template.cpp
@@ -0,0 +1,9 @@
+//
+//  template.cpp
+//  c++_xcode
+//
+//  Created by Andrew on 2/17/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "template.hpp"
---
+++ xcode/c++_xcode/template.hpp
@@ -0,0 +1,14 @@
+//
+//  template.hpp
+//  c++_xcode
+//
+//  Created by Andrew on 2/17/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef template_hpp
+#define template_hpp
+
+#include <stdio.h>
+
+#endif /* template_hpp */
