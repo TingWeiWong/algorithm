0f5978f4808c59d126bd45111926f40085f5bd36
Andrew Wong <andrew.wong.0101@gmail.com>
2019-02-18 22:32:33 +0800

    Add STL library

---
+++ xcode/c++_xcode/STL.cpp
@@ -0,0 +1,12 @@
+//
+//  STL.cpp
+//  c++_xcode
+//
+//  Created by Andrew  on 2/18/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#include "STL.hpp"
+#include <iostream>
+using namespace std;
+
---
+++ xcode/c++_xcode/STL.hpp
@@ -0,0 +1,14 @@
+//
+//  STL.hpp
+//  c++_xcode
+//
+//  Created by Andrew  on 2/18/19.
+//  Copyright © 2019 Andrew . All rights reserved.
+//
+
+#ifndef STL_hpp
+#define STL_hpp
+
+#include <stdio.h>
+
+#endif /* STL_hpp */
