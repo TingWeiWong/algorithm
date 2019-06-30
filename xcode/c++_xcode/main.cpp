315e7810f12c6cfdf02b381925a831289578020f
Andrew <andrew.wong.0101@gmail.com>
2019-06-30 15:32:31 +0800

    Test data pointer

--- xcode/c++_xcode/main.cpp
+++ xcode/c++_xcode/main.cpp
@@ -75,7 +75,11 @@ int main() {
     cout << "Test = " << test << endl;
     cout << "LCS dynamic = " << lcs_dynamic("abcs", "iasc") << endl;
     cout << "LIS dynamic = " << lis_dynamic({1,2,3,4,1,2,5,1,2,4}) << endl;
-    factorization(12);
+//    factorization(12);
+    
+    vector<string> data = {"A","B","C"};
+    cout << "data 0 = " << &data[0] << endl;
+    
 //    cout << "Sum of Array = " << simpleArraySum({1,2,3,4,5,6}) << endl;
 //    cout << "Discrete 1 = "  << disc1(3) << endl;
 //    cout << "Discrete 2 = "  << disc2(3) << endl;
